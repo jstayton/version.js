9f84ddbdd06784dc38fb8832d03644d8e5058883
Justin Stayton <justin.stayton@gmail.com>
2013-03-24 18:08:58 -0500

    Updated test spec to test 'version.test'.

--- test/spec/spec.js
+++ test/spec/spec.js
@@ -1,31 +1,57 @@
 describe('Version.js', function () {
   'use strict';
 
-  var queryVersion = function () {
-    var match = /[&\?]versionjs=([^&]+)/.exec(window.location.search);
+  var queryVersion = function (param) {
+    var regex = new RegExp('[&\\?]' + param + '=([^&]+)'),
+        match = regex.exec(window.location.search);
 
     return match && match[1];
   };
 
-  it('should load a script', function () {
-    expect($).toBeDefined();
+  describe('Loading with `data` attributes', function () {
+    it('should load a script', function () {
+      expect($).toBeDefined();
+    });
+
+    it('should load the default script version when no query string', function () {
+      if (queryVersion('versionjs')) {
+        return true;
+      }
+
+      expect($().jquery).toEqual('1.9.1');
+    });
+
+    it('should load the script version specified by the query string', function () {
+      var version = queryVersion('versionjs');
+
+      if (!version) {
+        return true;
+      }
+
+      expect($().jquery).toEqual(version);
+    });
   });
 
-  it('should load the default script version when no `versionjs` query string', function () {
-    if (queryVersion()) {
-      return true;
-    }
+  describe('Loading with `version.load`', function () {
+    it('should load a script', function () {
+      runs(function () {
+        version.load({
+          url: 'cdnjs',
+          library: 'lodash.js',
+          version: '1.0.1',
+          file: 'lodash.min',
+          param: 'lodash'
+        });
+      });
 
-    expect($().jquery).toEqual('1.9.1');
-  });
+      waitsFor(function () {
+        return typeof _ !== 'undefined';
+      }, 'The script should be loaded', 5000);
 
-  it('should load the script version specified by the `versionjs` query string', function () {
-    var version = queryVersion();
-
-    if (!version) {
-      return true;
-    }
-
-    expect($().jquery).toEqual(version);
+      runs(function () {
+        expect(_).toBeDefined();
+        expect(_.VERSION).toEqual('1.0.1');
+      });
+    });
   });
 });
